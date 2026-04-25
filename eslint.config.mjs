af881acd43d35b47111a5f4c92e9d28bca074ae1
handikade <dika.csui@gmail.com>
2026-04-25 13:31:11 +0700

    chore: ignore @generated from linter & formatter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', '__plan/**/*'],
+    ignores: ['eslint.config.mjs', '__plan/**/*', 'src/@generated/**/*'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
