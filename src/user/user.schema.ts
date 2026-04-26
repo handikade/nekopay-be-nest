abd4b74f64063444d14d8dcc4f56dea589c92194
handikade <dika.csui@gmail.com>
2026-04-26 21:36:46 +0700

    feat(user): schema

---
+++ src/user/user.schema.ts
@@ -0,0 +1,25 @@
+import { SortOrderSchema, UserScalarFieldEnumSchema, UserSchema } from '@prisma/zod';
+import { z } from 'zod';
+
+export const UserCreateSchema = UserSchema.omit({
+  id: true,
+  password: true,
+  created_at: true,
+  updated_at: true,
+});
+
+export const UserQueryParamsSchema = z.object({
+  page: z.coerce.number().default(1).describe('Page number'),
+  limit: z.coerce.number().default(10).describe('Number of items per page'),
+  search: z.string().optional().describe('Search keyword'),
+  sortBy: UserScalarFieldEnumSchema.optional().default('created_at'),
+  sortOrder: SortOrderSchema.optional().default('desc'),
+});
+
+export const UserPresentationSchema = UserSchema.omit({
+  password: true,
+  created_at: true,
+  updated_at: true,
+});
+
+export const UserListSchema = z.array(UserPresentationSchema);
